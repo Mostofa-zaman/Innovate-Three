3a9b59bb54dfb082637d50f8cf02a4d7b94fce5d
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-07 23:53:28 +0600

    responsive banner part

--- src/component/Bannar.jsx
+++ src/component/Bannar.jsx
@@ -1,98 +1,102 @@
-import React from "react";
-import Button from "./Button";
-import Cross from "./Cross";
-import MotionOne from "./MotionOne";
-import Dot from "./Dot";
-
-// impor for images
-import BannarImages from "../assets/bannar.png";
-import DotOne from "../assets/dotline.png";
-import DotTwo from "../assets/dotline2.png";
-import Puse from "../assets/puse.png";
-import TailOne from "../assets/tail1.png";
-import TailTwo from "../assets/tail2.png";
-import Ring from "./Ring";
-
 const Bannar = () => {
   return (
-    <section className="pb-[120px] pt-[180px] relative overflow-hidden">
-      <div className="absolute  right-[32%] top-[120px] -translate-x-1/2">
+    <section className="pb-[80px] md:pb-[120px] pt-[120px] md:pt-[180px] relative overflow-hidden">
+      
+      {/* Decorative elements – hide on small devices */}
+      <div className="hidden lg:block absolute right-[32%] top-[120px] -translate-x-1/2">
         <Ring className={"border-[#FF7628]"} />
       </div>
-      <div className="absolute  left-[20.5%] bottom-[130px] -translate-x-1/2">
+
+      <div className="hidden lg:block absolute left-[20.5%] bottom-[130px] -translate-x-1/2">
         <Ring className={"border-[#8773EF]"} />
       </div>
-      <div className="absolute right-[0%] top-[180px] ">
+
+      <div className="hidden lg:block absolute right-0 top-[180px]">
         <img src={TailOne} alt="TailOne" />
       </div>
-      <div className="absolute left-[0%] bottom-[0px] ">
+
+      <div className="hidden lg:block absolute left-0 bottom-0">
         <img src={TailTwo} alt="TailTwo" />
       </div>
-      <div className="absolute left-[20%] bottom-[140px] -translate-x-1/2">
+
+      <div className="hidden lg:block absolute left-[20%] bottom-[140px] -translate-x-1/2">
         <Dot className={"bg-[#FCDA68]"} />
       </div>
-      <div className="absolute right-[28%] bottom-[210px] -translate-x-1/2">
+
+      <div className="hidden lg:block absolute right-[28%] bottom-[210px] -translate-x-1/2">
         <Dot className={"bg-rose-400"} />
       </div>
-      <div className="absolute right-[32%] top-[160px] -translate-x-1/2">
+
+      <div className="hidden lg:block absolute right-[32%] top-[160px] -translate-x-1/2">
         <Dot className={"bg-green-400"} />
       </div>
-      <div className="absolute left-[40%] top-[210px] -translate-x-1/2">
+
+      <div className="hidden lg:block absolute left-[40%] top-[210px] -translate-x-1/2">
         <img src={Puse} alt="Puse" />
       </div>
-      <div className="absolute bottom-[208px] left-[50%] -translate-x-1/2  ">
+
+      <div className="hidden lg:block absolute bottom-[208px] left-1/2 -translate-x-1/2">
         <MotionOne />
       </div>
-      <div className="absolute top-[-50px] left-[-60px]">
+
+      <div className="hidden lg:block absolute top-[-50px] left-[-60px]">
         <img src={DotOne} alt="DotOne" />
       </div>
-      <div className="absolute top-[-15px] left-[50%] -translate-x-1/2">
-        <img src={DotTwo} alt="DotOne" />
+
+      <div className="hidden lg:block absolute top-[-15px] left-1/2 -translate-x-1/2">
+        <img src={DotTwo} alt="DotTwo" />
       </div>
-      <div className={" absolute top-[50px] right-[50px]"}>
+
+      <div className="hidden lg:block absolute top-[50px] right-[50px]">
         <Cross />
       </div>
-      <div className="absolute flex gap-x-2 top-[50%] -translate-y-1/2 left-[100px]">
+
+      <div className="hidden lg:flex absolute gap-x-2 top-1/2 -translate-y-1/2 left-[100px]">
         <Cross className={"w-[12px]"} />
         <Cross className={"w-[12px]"} />
         <Cross className={"w-[12px]"} />
       </div>
 
-      <div className="max-w-[1320px] mx-auto">
-        <div className="grid grid-cols-5  items-center ">
-          <div className="col-span-3">
-            <h1 className="font-opensans font-extrabold text-[63px] text-primary">
-              {" "}
+      {/* Content */}
+      <div className="max-w-[1320px] mx-auto px-4">
+        <div className="grid grid-cols-1 lg:grid-cols-5 items-center gap-y-12">
+          
+          {/* Text */}
+          <div className="lg:col-span-3 text-center lg:text-left">
+            <h1 className="font-opensans font-extrabold text-[36px] md:text-[48px] lg:text-[63px] text-primary">
               We build E-commerce solution your business
             </h1>
-            <p className="font-opensans font-normal text-[20px]/[40px] text-secondary w-[571px] py-8">
+
+            <p className="font-opensans font-normal text-[16px] md:text-[18px] lg:text-[20px]/[40px] text-secondary 
+              max-w-full lg:w-[571px] mx-auto lg:mx-0 py-6 lg:py-8">
               Use customer data to build great and solid product experiences
               that convert. Digital marketing’s development has changed the way
               brands and businesses use technology for marketing.
             </p>
 
-            <div className="flex gap-x-[20px]">
+            <div className="flex justify-center lg:justify-start gap-x-[20px]">
               <Button
                 buttonName={"Contact Us"}
-                className={
-                  "bg-transparent text-primary border  border-[#8454F5] hover:bg-[#8454F5] hover:text-[#fff] transition duration-300 ease-in-out "
-                }
+                className="bg-transparent text-primary border border-[#8454F5] hover:bg-[#8454F5] hover:text-white transition duration-300"
               />
               <Button
                 buttonName={"Learn More"}
-                className={
-                  "bg-transparent text-primary border  border-[#8454F5] hover:bg-[#8454F5] hover:text-[#fff] transition duration-300 ease-in-out"
-                }
+                className="bg-transparent text-primary border border-[#8454F5] hover:bg-[#8454F5] hover:text-white transition duration-300"
               />
             </div>
           </div>
-          <div className=" col-span-2">
-            <img src={BannarImages} alt="BannarImages" className="h-[600px] " />
+
+          {/* Image */}
+          <div className="lg:col-span-2 flex justify-center">
+            <img
+              src={BannarImages}
+              alt="BannarImages"
+              className="h-[300px] md:h-[450px] lg:h-[600px]"
+            />
           </div>
+
         </div>
       </div>
     </section>
   );
 };
-
-export default Bannar;

a81b844bb3d6b1ac59fc4c74e4c72fef204c78ff
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-08 23:57:55 +0600

    responsive selection card

--- src/component/SelectionCard.jsx
+++ src/component/SelectionCard.jsx
@@ -12,12 +12,12 @@ const SelectionCard = ({
       <img
         src={imageName}
         alt="imageName selection Card"
-        className={`w-full bg-cover  object-cover  ${imageClassName}`}
+        className={`w-full bg-cover object-cover ${imageClassName} h-auto sm:h-[400px] md:h-[500px] lg:h-auto`}
       />
       <div
-        className={`bg-[#E2DDFB] py-[15px] px-[30px] w-[295px] absolute group-hover:bg-[#FEF8E4] transition duration-300 ease-in-out ${batchOverlayClassName} `}
+        className={`bg-[#E2DDFB] py-3 px-4 sm:py-4 sm:px-6 w-[250px] sm:w-[295px] absolute group-hover:bg-[#FEF8E4] transition duration-300 ease-in-out ${batchOverlayClassName}`}
       >
-        <h6 className="font-opensans font-bold text-[24px]/[36px] text-primary ">
+        <h6 className="font-opensans font-bold text-[18px] sm:text-[20px] lg:text-[24px] text-primary">
           {heading}
         </h6>
       </div>
